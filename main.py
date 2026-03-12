c1dec867d8cd934a4d476a220134816ddfc0858f
rileyscag09-hash <rileyscag09@gmail.com>
2026-03-12 23:36:06 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Entry point for the application.
+"""
+import asyncio
+import sys
+
+from UEC import run
+from utils.constants import logger
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(run())
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+        sys.exit()
+    except Exception as e:
+        logger.critical(e)
