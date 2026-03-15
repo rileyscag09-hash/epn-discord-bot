e796044239e85a6c1ddae2384dab526fccb2ece3
nicknamebot <nuggetperson2@gmail.com>
2026-03-15 21:46:06 +0000

    Fix embed variable for user ban notification

--- cogs/commands/epn.py
+++ cogs/commands/epn.py
@@ -334,12 +334,12 @@ class EPNCommands(commands.Cog):
                     title="User Blacklisted",
                     description=f"**{user.display_name}** has been added to the EPN blacklist."
                 )
-                embed = EmbedDesign.create_embed(
+                embed1 = EmbedDesign.create_embed(
                     title="You have been blacklisted in ER:LC Partner Network",
                     description=f"Hello, **{user.display_name}**. You have been banned from EPN for {reason}"
                 )
                 await interaction.followup.send(embed=embed)
-                await user.send(embed=embed)
+                await user.send(embed=embed1)
                 await self.send_ban_notification("ban", user, reason, interaction.user, "Cross-Guild Ban", evidence, expires_at, appealable)
 
             except Exception as e:
